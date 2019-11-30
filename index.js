6226fc4ce55ef4b6923e6049e14aaadaf361b4ac
osubou <oussama.boukich@insa-lyon.fr>
2019-11-30 14:34:58 +0100

    commit 2

---
+++ index.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var bodyParser= require("body-parser");
+var DataStore= require("nedb");
+var port = 3000;
+var BASE_API_PATH="/api/v1";
+var DB_FILE_NAME = __dirname + "/contacts.json";
+console.log("Starting API server...");
+
+var app= express();
+app.use(bodyParser.json());
+
+var db = new DataStore({
+    filename: DB_FILE_NAME,
+    autoload: true
+});
+
+app.get("/", (req,res) =>{
+    res.send("<html><body><h1> My server</h1></body></html>");
+} );
+app.get(BASE_API_PATH + "/contacts",(req,res)=>{
+    console.log(Date() + "-GET/contacts");
+    res.send([]);
+} );
+
+app.post(BASE_API_PATH +"/contacts",(req,res)=>{
+    console.log(Date() +"- POST/contacts");
+    var contact=req.body;
+   
+    db.insert(contact, (err)=>{
+        if(err){
+            console.log(Date() + "-" + err);
+            res.sendStatus(500);
+        }else{
+            res.sendStatus(201);
+        }
+    });
+});
+
+
+app.listen(port);
+
+console.log("Server ready")
